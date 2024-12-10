901f7cb79091eaf0cf3e41b38d97d753ec669579
Bostoncool_Desktop <lnuo08951@gmail.com>
2024-12-10 22:27:34 +0800

    rename

--- LSTM/Data Preprocessing.py
+++ LSTM/Data Preprocessing.py
@@ -1,5 +1,6 @@
 from pandas import read_csv
 import pandas as pd
+import numpy as np
 
 # 首先读取数据，不进行日期解析
 dataset = read_csv(r'C:\Users\IU\Desktop\something\Long-short term memory\PRSA_Data\PRSA_data_2010.1.1-2014.12.31.csv')
@@ -20,8 +21,22 @@ dataset.drop(['No', 'year', 'month', 'day', 'hour'], axis=1, inplace=True)
 # 设置列名
 dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
 
-# 填充缺失值
-dataset['pollution'].fillna(0, inplace=True)
+# 检查缺失值的情况
+print("缺失值统计：")
+print(dataset.isna().sum())
+
+# 检查是否存在字符串形式的'NA'
+print("\n'NA'字符串的数量：")
+print((dataset == 'NA').sum())
+
+# 更全面的缺失值处理
+dataset['pollution'] = dataset['pollution'].replace('NA', np.nan)  # 将字符串'NA'转换为np.nan
+dataset['pollution'] = pd.to_numeric(dataset['pollution'], errors='coerce')  # 确保数据类型为数值型
+dataset['pollution'].fillna(0, inplace=True)  # 填充缺失值
+
+# 再次检查缺失值情况
+print("\n处理后的缺失值统计：")
+print(dataset.isna().sum())
 
 # 删除前24小时的数据
 dataset = dataset[24:]
