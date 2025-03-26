54b12a74f815bbaf31c3be9a1626f7df35b85ff0
Bostoncool_Desktop <lnuo08951@gmail.com>
2025-03-26 21:09:14 +0800

    update

--- CNN/Cuda and cuDNN.py
+++ CNN/Cuda and cuDNN.py
@@ -1,4 +1,7 @@
 import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
-print(torch.version.cuda)
+print(f"PyTorch Version: {torch.__version__}")
+print(f"CUDA is available: {torch.cuda.is_available()}")
+print(f"CUDA Version: {torch.version.cuda}")
+
+# 三种Python的版本，需要不同的方式进行调度，好在是完成了。
+# 最开始的时候是需要手动添加path，然后进行安装的。
